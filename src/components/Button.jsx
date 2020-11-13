f1c7034e8e71e0ae90e450743488d491bff1864c
pheeria <olzhas.askar@deliveryhero.com>
2020-11-13 16:58:34 +0100

    style: things

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+import React from "react";
+import styled, { css } from "styled-components";
 
 const outlinedStyle = css`
-color: #ed2186;
-background-color: transparent;
-&:hover {
-  background-color: #af1e6630;
-}
+  color: #ed2186;
+  background-color: transparent;
+  &:hover {
+    background-color: #af1e6630;
+  }
 `;
 
 export const Button = styled.button`
   cursor: pointer;
   padding: 8px 16px;
   font-size: 1em;
-  background-color: #ed2186;
+  background-color: #4787ff;
   color: #fff;
-  border: 1px solid #af1e66;
-  
+  border: 1px solid #6347ff;
+
   &:hover {
-    background-color: #af1e66;
+    background-color: #6347ff;
   }
 
-  ${props => props.variant === 'outline' && outlinedStyle}
-  ${props => props.variant === 'dotted' && css`
-    ${outlinedStyle}
-    border-style: dotted;
-  `}
+  ${(props) => props.variant === "outline" && outlinedStyle}
+  ${(props) =>
+    props.variant === "dotted" &&
+    css`
+      ${outlinedStyle}
+      border-style: dotted;
+    `}
 
   outline: 0;
   border-radius: 6px;
   transition: background-color 0.5s;
-
-  
 `;
